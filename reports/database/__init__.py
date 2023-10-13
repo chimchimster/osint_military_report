0871df2757df254950cdfe660e8ebb80f3bbf404
chimchimster <jorddyishere@gmail.com>
2023-10-13 12:54:39 +0600

    changed database for exel handler in one sql query

--- reports/database/__init__.py
+++ reports/database/__init__.py
@@ -2,7 +2,6 @@ from .handlers import *
 from .models import *
 
 __all__ = [
-    'get_normal_users_data_mapped_to_moderator',
-    'get_destructive_users_data_mapped_to_moderator',
+    'get_users_data_mapped_to_moderator',
     'UserProfile',
 ]
