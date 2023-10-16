285af858256038dbb377b0f7c7f743be1431e6ff
chimchimster <jorddyishere@gmail.com>
2023-10-16 14:59:05 +0600

    propagate logic

--- reports/tasks.py
+++ reports/tasks.py
@@ -1,12 +1,21 @@
 import asyncio
+import uuid
+from datetime import datetime
 
 from pathlib import Path
-from typing import List
+from typing import List, Final
+
+from fastapi.responses import FileResponse
 
 from .models import ClientSettings
 from .database import *
 from .xlsx_report import *
 from .dataframes import *
+from .utils import *
+
+
+REPORT_STACK_LOCK = asyncio.Lock()
+PREVIOUS_REPORTS_STACK: Final[List[str]] = []
 
 
 class ReportDistributor:
@@ -15,10 +24,10 @@ class ReportDistributor:
 
     path_to_temp: Path = Path.cwd() / 'reports' / 'temp'
 
-    def __init__(self, resp_obj: ClientSettings, data: List[List]):
+    def __init__(self, resp_obj: ClientSettings):
         self.resp_obj = resp_obj
 
-    async def generate_report(self):
+    async def generate_report(self) -> FileResponse:
         """ r_type: тип отчета. 1 - пользователи, 2 - подписки.
             r_format: формат отчета. xlsx, pdf, pptx, csv.
             user_id: идентификатор модератора в системе. """
@@ -27,17 +36,20 @@ class ReportDistributor:
         r_format = self.resp_obj.r_format
         user_id = self.resp_obj.user_id
 
+        report_file_name: str = str(uuid.uuid4()) + '.' + r_format
+        report_path: Path = self.path_to_temp / report_file_name
+
         if r_type == 1:
 
             users_data = await get_users_data_mapped_to_moderator(user_id)
 
-            dataframe = generate_dataframe_for_users(users_data)
+            dataframe = await generate_dataframe_for_users(users_data)
 
         elif r_type == 2:
 
             subscriptions_data = await get_subscriptions_data_mapped_to_moderator(user_id)
 
-            dataframe = generate_dataframe_for_subscriptions(subscriptions_data)
+            dataframe = await generate_dataframe_for_subscriptions(subscriptions_data)
 
         else:
             raise RuntimeError(f'Непредусмотренный тип {r_type} отчета.')
@@ -46,6 +58,21 @@ class ReportDistributor:
 
         if r_format == 'xlsx':
 
-            await running_loop.run_in_executor(None, render_xlsx_document, dataframe, self.report_path)
+            await running_loop.run_in_executor(None, render_xlsx_document, dataframe, report_path)
 
+        filename = f'Отчет от {datetime.now()} военнослужащие.{r_format}'
 
+        response_file = FileResponse(
+            path=report_path,
+            filename=filename,
+            media_type='multipart/form-data',
+        )
+
+        async with REPORT_STACK_LOCK:
+            if PREVIOUS_REPORTS_STACK:
+                prev_report_name = PREVIOUS_REPORTS_STACK.pop()
+                await remove_report(self.path_to_temp, prev_report_name)
+
+            PREVIOUS_REPORTS_STACK.append(report_file_name)
+
+        return response_file
--- reports/xlsx_report.py
+++ reports/xlsx_report.py
@@ -5,16 +5,15 @@ from pandas import DataFrame, ExcelWriter
 def render_xlsx_document(
         dataframe: DataFrame,
         report_path: Path,
-        sheet_name: str,
 ) -> None:
 
     try:
         with ExcelWriter(report_path, engine='xlsxwriter', mode='w') as writer:
-            dataframe.to_excel(writer, sheet_name=sheet_name, index=False)
+            dataframe.to_excel(writer, sheet_name='военнослужащие', index=False)
             for column in dataframe:
                 column_width = max(dataframe[column].astype(str).map(len).max(), len(column)) + 1
                 col_idx = dataframe.columns.get_loc(column)
-                writer.sheets[sheet_name].set_column(col_idx, col_idx, column_width)
+                writer.sheets['военнослужащие'].set_column(col_idx, col_idx, column_width)
 
     except FileNotFoundError:
         raise FileNotFoundError(f'По указанному пути {report_path} ничего не найдено!')
